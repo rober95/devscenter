f238c6dbb12b9232460e9760437f6329547e6a9d
Roberto <robert.9524@gmail.com>
2020-06-12 12:15:00 -0400

    Initial react setup and cleanup

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -15,11 +15,6 @@ const {
   deleteProfile,
 } = require('../../controllers/profile');
 
-// @route   GET api/profile/test
-// @desc    Test profile route
-// @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
-
 router
   .route('/')
   .get(passport.authenticate('jwt', { session: false }), currentUserProfile)
