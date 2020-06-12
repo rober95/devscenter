e0def4a3551a5456e2a9f302b8a00db430bb8542
Roberto <robert.9524@gmail.com>
2020-06-12 15:06:50 -0400

    Error handling for register

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 exports.mongoURI =
-  'mongodb+srv://rober95:258540fc@contactkeeper-z6s6a.mongodb.net/devconnector?retryWrites=true&w=majority';
+  'mongodb+srv://rober95:258540fc@contactkeeper-z6s6a.mongodb.net/devscenter?retryWrites=true&w=majority';
 
 exports.secretOrKey = 'someJwtSecretThingy';
