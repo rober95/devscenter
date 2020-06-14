186844c84bf25111d7b6c9310d38c66631f385d5
Roberto <robert.9524@gmail.com>
2020-06-14 10:55:37 -0400

    Profile Header, About, Credentials and Github components added

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -32,6 +32,7 @@ class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.website}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fas fa-globe fa-2x"></i>
                   </a>
@@ -41,6 +42,7 @@ class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.twitter}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-twitter fa-2x"></i>
                   </a>
@@ -50,6 +52,7 @@ class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.facebook}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-facebook fa-2x"></i>
                   </a>
@@ -59,6 +62,7 @@ class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.linkedin}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-linkedin fa-2x"></i>
                   </a>
@@ -68,6 +72,7 @@ class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.youtube}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-youtube fa-2x"></i>
                   </a>
@@ -77,6 +82,7 @@ class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.instagram}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-instagram fa-2x"></i>
                   </a>
