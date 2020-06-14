9e734b05f3290d93c44308d1b04c31b0c0608f7b
Roberto <robert.9524@gmail.com>
2020-06-13 21:28:59 -0400

    Profiles component and getProfiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -6,6 +6,7 @@ import {
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
   SET_CURRENT_USER,
+  GET_PROFILES,
 } from './types';
 
 // Get current profile
@@ -98,6 +99,25 @@ export const deleteEducation = (id, history) => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data,
+      })
+    );
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data,
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null,
       })
     );
 };
