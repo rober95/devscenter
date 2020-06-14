fd426e248618004b521c0652b0badf76f8da673b
Roberto <robert.9524@gmail.com>
2020-06-14 11:42:31 -0400

    Post state setup and addPost action added

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,16 @@
+// import {
+
+// } from '../actions/types';
+
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
