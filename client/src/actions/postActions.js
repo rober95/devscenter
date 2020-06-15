38607811001b28a0d198906a768c4cbd2e7ed802
Roberto <robert.9524@gmail.com>
2020-06-14 20:40:15 -0400

    added CommentFeed and CommentItem components to display the comments

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -7,10 +7,12 @@ import {
   POST_LOADING,
   DELETE_POST,
   GET_POST,
+  CLEAR_ERRORS,
 } from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post('/api/posts', postData)
     .then(res =>
@@ -111,8 +113,27 @@ export const removeLike = id => dispatch => {
 
 // Add Comment
 export const addComment = (postId, newComment) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, newComment)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
     .then(res =>
       dispatch({
         type: GET_POST,
@@ -131,5 +152,12 @@ export const addComment = (postId, newComment) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING,
+  };
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS,
   };
 };
