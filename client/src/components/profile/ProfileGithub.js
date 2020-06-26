8786526934c0d31c419bb9dcbffd84acfa5c1740
Roberto <robert.9524@gmail.com>
2020-06-26 17:09:32 -0400

    Fixed bug when no GitHub repos found

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -26,29 +26,36 @@ const ProfileGithub = ({ username }) => {
     })(); //eslint-disable-next-line
   }, []);
 
-  const repoItems = repos.map(repo => (
-    <div key={repo.id} className="card card-body mb-2">
-      <div className="row">
-        <div className="col-md-6">
-          <h4>
-            <Link to={repo.html_url} className="text-info" target="_blank">
-              {repo.name}
-            </Link>
-          </h4>
-          <p>{repo.description}</p>
+  const repoItems =
+    repos.message === 'Not Found' ? (
+      <p>No GitHub repos found for this user</p>
+    ) : (
+      repos.map(repo => (
+        <div key={repo.id} className="card card-body mb-2">
+          <div className="row">
+            <div className="col-md-6">
+              <h4>
+                <Link to={repo.html_url} className="text-info" target="_blank">
+                  {repo.name}
+                </Link>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div className="col-md-6">
+              <span className="badge badge-info mr-1">
+                Stars: {repo.stargazers_count}
+              </span>
+              <span className="badge badge-secondary mr-1">
+                Watchers: {repo.watchers_count}
+              </span>
+              <span className="badge badge-success">
+                Forks: {repo.forks_count}
+              </span>
+            </div>
+          </div>
         </div>
-        <div className="col-md-6">
-          <span className="badge badge-info mr-1">
-            Stars: {repo.stargazers_count}
-          </span>
-          <span className="badge badge-secondary mr-1">
-            Watchers: {repo.watchers_count}
-          </span>
-          <span className="badge badge-success">Forks: {repo.forks_count}</span>
-        </div>
-      </div>
-    </div>
-  ));
+      ))
+    );
 
   return (
     <div ref={myRef}>
